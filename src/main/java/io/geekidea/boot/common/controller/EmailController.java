d11afebc6f8313f20a3aedafac6a58869800e220
snailuu <67217277@qq.com>
2024-06-17 20:18:44 +0800

    删除mq

--- src/main/java/io/geekidea/boot/common/controller/EmailController.java
+++ src/main/java/io/geekidea/boot/common/controller/EmailController.java
@@ -1,8 +1,6 @@
 package io.geekidea.boot.common.controller;
 
 import io.geekidea.boot.common.compoent.SendEmail;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +12,6 @@ public class EmailController {
     @Resource
     private SendEmail sendEmail;
 
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
 
     @GetMapping("/sendEmail/{email}")
     public String sendEmail(@PathVariable("email") String email) {
