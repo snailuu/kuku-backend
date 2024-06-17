2441bc3413d584de3e259dc018682b6120b761ed
snailuu <67217277@qq.com>
2024-06-17 22:26:38 +0800

    docs：更新文档

--- src/main/java/io/geekidea/boot/test/controller/DataInfoController.java
+++ src/main/java/io/geekidea/boot/test/controller/DataInfoController.java
@@ -3,12 +3,14 @@ package io.geekidea.boot.test.controller;
 import io.geekidea.boot.framework.response.ApiResult;
 import io.geekidea.boot.test.service.DataInfoService;
 import io.geekidea.boot.test.vo.DataInfoVo;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/data")
+@Tag(name = "首页数据查询")
 public class DataInfoController {
 
     @Autowired
